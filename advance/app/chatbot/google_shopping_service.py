703564792393d48b69767dd6882571e6a1ede7e3
Ni Ngo <ni.ngo@asnet.com.vn>
2024-10-09 17:23:49 +0700

    Load json directly from env

--- advance/app/chatbot/google_shopping_service.py
+++ advance/app/chatbot/google_shopping_service.py
@@ -14,43 +14,26 @@ logging.info("This is an info log message")
 import json
 import io
 import ast
-data = json.loads(os.environ['GOOGLE_API_KEY'])
+# data = json.loads(os.environ['GOOGLE_API_KEY'])
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.abspath(os.path.join(current_dir, 'assets/google_api_key.json'))
-with io.open(file_path, 'w', encoding='utf8') as outfile:
-    json.dump(data, outfile)
+# with io.open(file_path, 'w', encoding='utf8') as outfile:
+#     json.dump(data, outfile)
 
 # Read JSON file
-with open(file_path) as data_file:
-    data_string = data_file.read()
-data_loaded = json.loads(data_string)
+# with open(file_path) as data_file:
+#     data_loaded = json.load(data_file)
+data_loaded = json.loads(os.environ['GOOGLE_API_KEY'])
 logging.info(data_loaded)
-logging.info(data_loaded.keys())
-logging.info(file_path)
 logging.info(print(type(data_loaded)))
-
-content = dict(
-   type=data_loaded["type"], 
-   project_id=data_loaded["project_id"], 
-   private_key_id=data_loaded["private_key_id"], 
-   private_key=data_loaded["private_key"], 
-   client_email=data_loaded["client_email"], 
-   client_id=data_loaded["client_id"], 
-   auth_uri=data_loaded["auth_uri"], 
-   token_uri=data_loaded["token_uri"], 
-   auth_provider_x509_cert_url=data_loaded["auth_provider_x509_cert_url"], 
-   client_x509_cert_url=data_loaded["client_x509_cert_url"], 
-   universe_domain=data_loaded["universe_domain"]
-)
-
-logging.info(content.keys())
+logging.info(data_loaded.keys())
 
 class GoogleShoppingService:
   resources: List = None
   def __init__(self) -> None:
     # Load credentials from the service account JSON file (you'll need to create this from Google Cloud Console)
     
-    credentials = service_account.Credentials.from_service_account_info(content)
+    credentials = service_account.Credentials.from_service_account_info(data_loaded)
 
     # Build the Google Shopping API service
     service = build('content', 'v2.1', credentials=credentials)
