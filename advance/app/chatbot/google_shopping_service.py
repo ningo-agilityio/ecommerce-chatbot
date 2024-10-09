427b49070d8e26698b31cc7670298b7e4d245fcf
Ni Ngo <ni.ngo@asnet.com.vn>
2024-10-09 16:13:27 +0700

    Save data into json file

--- advance/app/chatbot/google_shopping_service.py
+++ advance/app/chatbot/google_shopping_service.py
@@ -9,14 +9,28 @@ import logging
 logging.basicConfig(level=logging.INFO)
 # Example of logging a message
 logging.info("This is an info log message")
+
+# Make it work for Python 2+3 and with Unicode
+import json
+import io
+data = json.loads(os.environ['GOOGLE_API_KEY'])
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.abspath(os.path.join(current_dir, 'assets/google_api_key.json'))
+# alternative_path = os.path.abspath(os.path.join(current_dir, 'assets/ecommercechatbot-436909-3af585186cb6.json'))
+with io.open(file_path, 'w', encoding='utf8') as outfile:
+    json.dump(data, outfile)
+
+# Read JSON file
+# with open(file_path) as data_file:
+#     data_loaded = json.load(data_file)
+# logging.info(data_loaded)
+
 class GoogleShoppingService:
   resources: List = None
   def __init__(self) -> None:
     # Load credentials from the service account JSON file (you'll need to create this from Google Cloud Console)
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.abspath(os.path.join(current_dir, "assets/ecommercechatbot-436909-2d9e145a5fa8.json"))
+    
     logging.info(file_path)
-    logging.info(os.environ['GOOGLE_MERCHANT_ID'])
     credentials = service_account.Credentials.from_service_account_file(file_path)
 
     # Build the Google Shopping API service
