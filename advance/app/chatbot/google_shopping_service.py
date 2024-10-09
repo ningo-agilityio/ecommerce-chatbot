e40097526132a35b75fcbfb230e6b4ef53581c51
Ni Ngo <ni.ngo@asnet.com.vn>
2024-10-09 16:57:19 +0700

    Create dict from json data

--- advance/app/chatbot/google_shopping_service.py
+++ advance/app/chatbot/google_shopping_service.py
@@ -13,6 +13,7 @@ logging.info("This is an info log message")
 # Make it work for Python 2+3 and with Unicode
 import json
 import io
+import ast
 data = json.loads(os.environ['GOOGLE_API_KEY'])
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.abspath(os.path.join(current_dir, 'assets/google_api_key.json'))
@@ -25,13 +26,31 @@ with open(file_path) as data_file:
 logging.info(data_loaded)
 # logging.info(data_loaded.keys())
 logging.info(file_path)
+logging.info(print(type(data_loaded))
+)
+
+content = dict(
+   type=data_loaded["type"], 
+   project_id=data_loaded["project_id"], 
+   private_key_id=data_loaded["private_key_id"], 
+   private_key=data_loaded["private_key"], 
+   client_email=data_loaded["client_email"], 
+   client_id=data_loaded["client_id"], 
+   auth_uri=data_loaded["auth_uri"], 
+   token_uri=data_loaded["token_uri"], 
+   auth_provider_x509_cert_url=data_loaded["auth_provider_x509_cert_url"], 
+   client_x509_cert_url=data_loaded["client_x509_cert_url"], 
+   universe_domain=data_loaded["universe_domain"]
+)
+
+logging.info(content.keys())
 
 class GoogleShoppingService:
   resources: List = None
   def __init__(self) -> None:
     # Load credentials from the service account JSON file (you'll need to create this from Google Cloud Console)
     
-    credentials = service_account.Credentials.from_service_account_info(data_loaded)
+    credentials = service_account.Credentials.from_service_account_info(content)
 
     # Build the Google Shopping API service
     service = build('content', 'v2.1', credentials=credentials)
