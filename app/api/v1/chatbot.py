d9e43935a8a26dc8f8e3eaa29087c5e11441272a
Ni Ngo <ni.ngo@asnet.com.vn>
2024-10-15 15:33:11 +0700

    Add stream to response

--- app/api/v1/chatbot.py
+++ app/api/v1/chatbot.py
@@ -1,6 +1,10 @@
+import logging
+from typing import Any
 from fastapi import APIRouter
 from pydantic import BaseModel, validator, Field
 from app.chatbot.agent_executor import run_with_memory
+from langchain.callbacks.base import BaseCallbackHandler
+from fastapi.responses import StreamingResponse
 
 router = APIRouter()
 
@@ -14,10 +18,27 @@ class QueryInput(BaseModel):
         if len(value) > 500:
             raise ValueError('Keyword exceeds 500 characters')
         return value
-    
+
+class StreamApiCallbackHandler(BaseCallbackHandler):
+    def __init__(self):
+        self.tokens = []
+
+    def on_llm_new_token(self, token: str, **kwargs):
+        # Collect each new token
+        logging.info(token)
+        self.tokens.append(token)
+
+    def token_generator(self):
+        # Yield tokens one by one as they are generated
+        for token in self.tokens:
+            yield token
+        self.tokens = []
+
 @router.post("/stream")
 def search_in_stream(input: QueryInput):
-    return 'Streaming response'
+    stream_handler = StreamApiCallbackHandler()
+    run_with_memory(input.keyword, stream_handler)
+    return StreamingResponse(stream_handler.token_generator(), media_type="text/plain")
 
 @router.post("/search")
 def search(input: QueryInput):
