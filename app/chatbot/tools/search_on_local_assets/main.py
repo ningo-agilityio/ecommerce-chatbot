da4a5fa7d53ec49c4331cc48d3ebcf2de87e5911
Ni Ngo <ni.ngo@asnet.com.vn>
2024-11-01 12:17:41 +0700

    Create a tool from retriever

--- app/chatbot/tools/search_on_local_assets/main.py
+++ app/chatbot/tools/search_on_local_assets/main.py
@@ -2,18 +2,12 @@
 import logging
 from typing import Any
 
-# Solve issue from Chroma when using Streamlit
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# import os
+# import openai
+# from dotenv import load_dotenv, find_dotenv
 
-import os
-import openai
-from dotenv import load_dotenv, find_dotenv
-
-_ = load_dotenv(find_dotenv()) # read local .env file
-openai.api_key = os.environ['OPENAI_API_KEY']
-
+# _ = load_dotenv(find_dotenv()) # read local .env file
+# openai.api_key = os.environ['OPENAI_API_KEY']
 
 # Langchain stuffs
 # from app.chatbot.react_agent import ReactAgentForLocalAssetsRouting
@@ -22,12 +16,14 @@ from langchain.retrievers.multi_vector import MultiVectorRetriever
 from langchain.storage import InMemoryByteStore
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables.passthrough import (
     RunnablePassthrough
 )
 from langchain_core.runnables.base import RunnableLambda
 from langchain.tools.retriever import create_retriever_tool
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain_openai import ChatOpenAI
+from langchain import hub
 
 # Separated built-in modules
 from app.chatbot.tools.search_on_local_assets.documents_loader import load_docs
@@ -54,8 +50,8 @@ def initialize_retriever_tool():
 
     tool_lookup_docs = create_retriever_tool(
         retriever=retriever,
-        name="order process",
-        description="Searches and returns excerpts from the FAQs, Order Process, Product Information, Returns and Refunds and Shipping Information.",
+        name="search_on_local_assets",
+        description="Search local assets (from vector store) for faqs (FAQs), order processes, returns, refunds, or shipping information. Sources: faqs.txt, order-process.json, returns-and-refunds.csv, shipping-info.txt.",
     )
     
     return tool_lookup_docs
@@ -93,11 +89,22 @@ def initialize_chain():
 
 class LookupLocalAssetsService:
   chain: Any
+  tool: Any
+  agent_executor: Any
   def __init__(self) -> None:
     
     self.chain = initialize_chain()
+    tool = initialize_retriever_tool()
+    self.tool = tool
+    # This will allow to test tool
+    # llm = ChatOpenAI(temperature=0.3, model="gpt-4o-mini",)
+    # prompt = hub.pull("hwchase17/openai-tools-agent")
+    # agent = create_openai_tools_agent(llm, [tool], prompt)
+    # self.agent_executor = AgentExecutor(agent=agent, tools=[tool])
 
   def search(self, query):
     return self.chain.invoke({
       "input": query,
     })
+# local_assets_service = LookupLocalAssetsService()
+# local_assets_service.search("Tell me about order process")
